b67ad4cc57346e8828b6ecfe2afb42dd59f52fd2
Kytel Flux <aoritb@outlook.com>
2019-11-27 13:33:13 +0000

    first version of react app

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="topBox">
+                <h1>Hello, I'm Adi</h1>
+            </div>
+
+            <div className="bounce-in-top">
+                 <span className="circle"></span>
+            </div>
+
+            <div className="bottomBox">
+                <h2>A web developer</h2>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Home
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function NavBar() {
+    return (
+        <nav className="container">
+            <a href="#">home</a>
+            <a href="#about me">about me</a>
+            <a href="#portfolio">portfolio</a>
+            <a href="#contact">contact</a>
+        </nav>
+    )
+}
+
+
+export default NavBar
