72fe7a07fc50c391514fac3527f37acc8d3ba377
Kytel Flux <aoritb@outlook.com>
2019-11-27 15:45:07 +0000

    Updates

---
+++ precache-manifest.ba54a4ecee1f22c428675467cd392eba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82d37b198e280b57507a7f0f7fa52c4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "24754634a9c5a3a46290",
+    "url": "/react-portfolio/static/css/main.4d8ce256.chunk.css"
+  },
+  {
+    "revision": "b37a36187b1f0adac5e1",
+    "url": "/react-portfolio/static/js/2.ccb790eb.chunk.js"
+  },
+  {
+    "revision": "24754634a9c5a3a46290",
+    "url": "/react-portfolio/static/js/main.da3bf4dd.chunk.js"
+  },
+  {
+    "revision": "52fa0a21dfdab6e515c3",
+    "url": "/react-portfolio/static/js/runtime-main.64dbd59a.js"
+  }
+]);
